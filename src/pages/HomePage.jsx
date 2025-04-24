22c065d3d1c3e0cbca3acee0584f2d7339593ae0
Narender <rainarender1997@gmail.com>
2025-04-24 13:37:31 +0530

    Your commit message here

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import HeroSection from "../components/Home/herosection";
-import CoursesList from "../components/home/CoursesList";
+import CoursesList from "@/components/home/CoursesList";
 import FeaturesSection from "../components/home/FeaturesSection";
 import TestimonialsSection from "../components/home/TestimonialsSection";
 import CtaSection from "../components/home/CtaSection"
