0b62427cc0ced38dc8dc93ac786b8209573cc952
Narender <rainarender1997@gmail.com>
2025-04-24 13:38:56 +0530

    Your commit message here

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-import HeroSection from "../components/Home/herosection";
+import HeroSection from "@/components/Home/herosection";
 import CoursesList from "@/components/home/CoursesList";
-import FeaturesSection from "../components/home/FeaturesSection";
-import TestimonialsSection from "../components/home/TestimonialsSection";
-import CtaSection from "../components/home/CtaSection"
+import FeaturesSection from "@/components/home/FeaturesSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
+import CtaSection from "@/components/home/CtaSection"
 
 const HomePage = () => {
   return (
