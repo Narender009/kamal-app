a9418a0dc8c1856ba8824391eaddc01518052e14
Narender <rainarender1997@gmail.com>
2025-04-28 10:47:57 +0530

    Initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import HeroSection from "../components/Home/herosection";
 import FeaturesSection from "../components/Home/FeaturesSection";
-import CoursesList from "../components/home/CoursesList";
-import TestimonialsSection from "../components/home/TestimonialsSection";
-import CtaSection from "../components/home/CtaSection"
+// import CoursesList from "../components/home/CoursesList";
+// import TestimonialsSection from "../components/home/TestimonialsSection";
+// import CtaSection from "../components/home/CtaSection"
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
-      <CoursesList />
+      {/* <CoursesList />
       <TestimonialsSection />
-      <CtaSection />
+      <CtaSection /> */}
     </div>
   )
 }
