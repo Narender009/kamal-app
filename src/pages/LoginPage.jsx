cae2bfcf731d98b6b087bfe4fc563c2f8e8a0463
Narender <rainarender1997@gmail.com>
2025-06-06 12:40:53 +0530

    Your update message

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Eye, EyeOff, Loader2 } from 'lucide-react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 
 const LoginPage = () => {
   const { login, isLoading } = useAuth();
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Eye, EyeOff, Loader2 } from 'lucide-react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterPage = () => {
